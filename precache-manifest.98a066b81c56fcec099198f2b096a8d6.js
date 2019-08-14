e380aefaffb9dda8922b2a11fadfa25e9b5ca3eb
Mitchell Hammack <hammackm1@gmail.com>
2019-08-13 21:28:34 -0400

    Updates

---
+++ precache-manifest.98a066b81c56fcec099198f2b096a8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2f514521991a9df83bba34a9d8df8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c191eb9479c53db6f5d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6ea4da12e04c7f054079",
+    "url": "/static/js/2.734d465a.chunk.js"
+  },
+  {
+    "revision": "9c191eb9479c53db6f5d",
+    "url": "/static/js/main.9be6f903.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
